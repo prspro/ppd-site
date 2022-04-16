e503a152f055005d3e2ecbac9dd82eac9cbb28cd
prospero <prospero1135@gmail.com>
2022-04-16 14:33:58 +0300

    css to sass fix and initial layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import './App.css';
+import './app.sass';
+import React from 'react';
+import { CssBaseline, Container } from '@material-ui/core';
 
 function App() {
   return (
-    <div className="App">
-      Test
+    <div className="app">
+      <CssBaseline />
+      <div className="app__col app__col--nav">
+        <Container maxWidth="sm">
+          test
+        </Container>
+      </div>
+      <div className="app__col app__col--main">
+        <Container maxWidth="lg">
+          test
+        </Container>
+      </div>
     </div>
   );
 }
